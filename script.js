c237ed4ebf0ad6faf0b39315499d8f2ba2ba871a
dedsec-ninja420 <66488101+dedsec-ninja420@users.noreply.github.com>
2022-04-18 22:20:35 +0200

    Script.js update
    Fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const button = document.getElementsByTagName("button")[0];
 const body = document.getElementsByTagName("body")[0];
+const h1 = document.getElementsByTagName("h1");
 const p = document.getElementsByTagName("p")[0];
 const hex = document.getElementsByTagName("span")[0];
 const colorInput = document.getElementById("color");
+const hidden = document.getElementsByClassName("hidden")[0];
 
 const generateBg = () => {
     let color = Math.floor(Math.random()*16777215).toString(16);
@@ -11,8 +13,20 @@ const generateBg = () => {
     colorInput.value = "#" + color;
 }
 
+const checkBlack = () => {
+    for (let i = 0; i < h1.length; i++) {
+        if (hex.textContent = "#000000") {
+            h1[i].style.color = "white";
+        } else if (hex.textContent != "#000000"){
+            h1[i].style.color = "black";
+        }   
+    }
+}
+
 const bgSet = () => {
+    console.log(colorInput.value)
     body.style.backgroundColor = colorInput.value;
+    hex.innerHTML  = colorInput.value;
 }
 
 button.addEventListener("click", generateBg)
